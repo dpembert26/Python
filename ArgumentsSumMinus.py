50eb107fa08c564fa4a617be394fddcf26df0d13
dpembert26 <dpembert26@gmail.com>
2017-02-13 22:01:53 -0500

    Python argparse

---
+++ ArgumentsSumMinus.py
@@ -0,0 +1,16 @@
+import argparse
+import operator
+
+parse = argparse.ArgumentParser(description = "This script will give the sum or the difference between two numbers.")
+parse.add_argument("x", type=int, help="First number.")
+parse.add_argument("y", type=int, help="Second number.")
+parse.add_argument("--sum",action="count", default=0)
+parse.add_argument("--minus", action="count", default=0)
+args = parse.parse_args()
+answer1 = args.x + args.y
+answer2 = args.x - args.y
+
+if args.sum:
+    print("{} plus {} is equals to {}".format(args.x, args.y , answer1))
+else:
+    print("{} minus {} is equals to {}".format(args.x, args.y, answer2))
