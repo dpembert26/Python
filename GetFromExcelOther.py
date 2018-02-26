e47c96494d3a222d327b5d5fb70c95d9d49dd5db
dpembert26 <dpembert26@users.noreply.github.com>
2018-02-25 19:28:30 -0500

    Excel Other

---
+++ GetFromExcelOther.py
@@ -0,0 +1,57 @@
+# Import modules
+import openpyxl
+import os
+filename = ""
+filename_dir = ""
+current_dir = ""
+
+# Function to gather information about the current working directory and file directory
+
+
+def get_current_dir():
+    global filename
+    filename = "House Things to do.xlsx"
+    global filename_dir
+    filename_dir = r"C:\Users\Darin\Documents"
+    global current_dir
+    current_dir = os.getcwd()
+    print("The current working directory is %s " % current_dir)
+
+# Function to set the current working directory to the file directory if they are not the same
+
+
+def get_set_working_dir():
+    if current_dir != filename_dir:
+        os.chdir(filename_dir)
+        working_dir = os.getcwd()
+        print("Changed the current directory to match the directory where the file is located: %s " % working_dir)
+    else:
+        print("Current directory matches the directory where the file is located: %s " % current_dir)
+
+# Function to read the information from the top columns and the rest of the excel and put them in a dictionary
+
+
+def read_excel():
+    workbook = openpyxl.load_workbook(filename)
+    sheet = workbook["Sheet1"]
+    col_arr = []
+    excel_obj = {}
+    max_row = sheet.max_row
+    for i in range(1, 6):
+        col_arr.append(sheet.cell(row=1, column=i).value)
+    for row_val in range(2, max_row + 1):
+        for col_val in range(1, 6):
+            excel_obj[col_arr[col_val - 1] + str(row_val)] = sheet.cell(row=row_val, column=col_val).value
+    print(excel_obj)
+
+# Main function
+
+           
+def main():
+    get_current_dir()
+    get_set_working_dir()
+    read_excel()
+
+
+# Call main function
+main()
