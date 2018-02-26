7e7028bcf2e70fd1133b190b9217de5bfeac8a49
dpembert26 <dpembert26@users.noreply.github.com>
2018-02-25 21:59:16 -0500

    Test Objects, List and Dictionaries

--- Professions.py
+++ Professions.py
@@ -1,5 +1,16 @@
+import re
+from itertools import islice
 given_name = ""
-person = []
+
+name_list = ["Darin Pemberton", "Larry Johnson", "Pete Michigan"]
+job_list = ["Automation Engineer", "Plumber", "Sales Executive"]
+skill_list = ["B+", "A", "C"]
+salary_list = ["$100,000", "$130,000", "$89,000"]
+
+
+def take(n, iterable):
+    # "Return first n items of the iterable as a list"
+    return list(islice(iterable, n))
 
 
 class Professions:
@@ -10,8 +21,12 @@ class Professions:
         self.salary = salary
 
     def say_who(self):
-        print("Here is the information about {}".format(self.name))
-        
+        global given_name
+        given_name = input("Please enter the full name of the person to lookup:\n")
+        if given_name != self.name:
+            print("The name was not found")
+        else:
+            print("Here is the information about {}".format(self.name))
 
     def print_info(self):
             print("Name: %s" % self.name)
@@ -22,14 +37,21 @@ class Professions:
 
 
 def main():
+    # combine multiple list into a dictionary
+    person_dict = dict((i[0],list(i[1:])) for i in zip(name_list,job_list,skill_list,salary_list))
+    n_list = take(1, person_dict.items()) # list of interable key/value pair from dictionary
+    # print(n_list)
+    person1 = Professions("Darin Pemberton", "Automation Engineer", "B", "$100,000")
+    person1.say_who()
+    person1.print_info()
 
-    person.append(Professions("Darin Pemberton","Automation Engineer", "B+", "$100,000"))
-    person.append(Professions("Larry Johnson", "Plumber", "A", "$130,000"))
-    person.append(Professions("Pete Michigan","Sales Executive", "B", "$89,000"))
+    person2 = Professions("Larry Johnson", "Plumber", "A" , "$130,000")
+    person2.say_who()
+    person2.print_info()
 
-    for p_list in person:
-        p_list.say_who()
-        p_list.print_info()
+    person3 = Professions("Pete Michigan", "Sales Executive", "C", "$89,000")
+    person3.say_who()
+    person3.print_info()
 
 
 main()
