9299f33915e777cfdd58b163b7b9ad7c9b6e4c2e
dpembert26 <dpembert26@users.noreply.github.com>
2018-02-26 23:43:42 -0500

    Simple dictionary

--- simple dictionary.py
+++ simple dictionary.py
@@ -3,11 +3,18 @@ states = {"NY":"New York","CO":"Colorado","CT":"Connecticut","TX":"Texas"}
 for key,value in states.items():
     print("%s is the abbreviation for the state of %s" % (key,value))
 
-print("These are the state abbreviations: ")
+print("\nThese are the state abbreviations: ")
 for key,value in states.items():
     print(key)
 
-print("These are the states proper names: ")
+print("\nThese are the states proper names: ")
 for key,value in states.items():
     print(value)
 
+print("\nAdding another state to the states dictionary")
+states["Other States"] = ["Georgia","Virginia","Hawaii","Florida"]
+print(states)
+
+print("\nPrint out all the other states")
+for key in states["Other States"]:
+    print(key)
