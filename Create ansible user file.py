02f9d820be46f86eae6ce56b52866fe4a6a787f1
dpembert26 <dpembert26@gmail.com>
2018-05-09 10:02:47 -0400

    Create ansible user file

--- Create ansible user file.py
+++ Create ansible user file.py
@@ -13,21 +13,42 @@ import json
 from io import StringIO
 
 # Path and filename for Ansible user file(yml)
-ansible_file_path = r"C:\Users\Darin\Downloads/"
+ansible_file_path = r"C:\Users\dpemberton\Documents/"
 file_name = r"users.yml"
 
 # Path and filename for username text file
-text_file_path = r"C:\Users\Darin\Downloads/"
-text_name = r"names.txt"
+text_file_path = r"C:\Users\dpemberton\Documents/"
+text_name = r"username.txt"
 
 
-# Empty list defined to collect usernames
+# Empty list defined to collect user names
 user_list = []
+
+
+# Function to delete the users.yml file if it exists
+def remove_yaml_file(file_path,yml_file):
+    try:
+        # Create full path for the file users.yml
+        full_path = os.path.join(file_path, yml_file)
+        # Check if users.yml file exist and close it
+        if os.path.isfile(full_path):
+            with open(full_path, 'r') as file:
+                file.close()
+                # remove the users.yml file
+                os.unlink(full_path)
+                print("The file {} has been deleted".format(yml_file))
+
+    except:
+        errors = sys.exc_info()
+        for idx, error in enumerate(errors):
+            if idx < 2:
+                print(error)
 
 
 # Function to create full paths for files
 def create_full_paths(dir,file):
     try:
+        # joining path with file
         full_path = os.path.join(dir,file)
         return full_path
 
@@ -38,6 +59,7 @@ def create_full_paths(dir,file):
                 print(error)
 
 
+# Function to get the contents of the username.txt file
 def get_text_contents(full_path):
     try:
         # File name for username.txt read
@@ -49,7 +71,7 @@ def get_text_contents(full_path):
                 username = m.group(1)
                 user_list.append(username)
                 count += 1
-            print("Count is {} and the List is {}".format(count,user_list))
+            print("There are {} user names to be setup and the List is {}".format(count,user_list))
         file.close()
 
     except:
@@ -61,8 +83,10 @@ def get_text_contents(full_path):
     return user_list
 
 
+# Function to build the Yaml file based on the username.txt file
 def build_yaml(users,full_path):
     try:
+        # Create file if it does not exist and write to it
         with open(full_path, 'w') as file:
             file.write("---\n")
             file.write("users:\n")
@@ -79,9 +103,13 @@ def build_yaml(users,full_path):
 
 
 def main():
+    # Close the users.yml file if it exists and then delete it
+    remove_yaml_file(ansible_file_path,file_name)
+
     # Creating full path for the username text file
     user_full_path = create_full_paths(text_file_path, text_name)
 
+    # Creating full path for the users.yml file
     ansible_full_path = create_full_paths(ansible_file_path, file_name)
 
     # Get the user names from the username file
